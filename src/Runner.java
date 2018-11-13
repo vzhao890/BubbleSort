1700d9f342a518e08d7af224bff5931c4337a013
BT_1N3_31@BKTECH.LOCAL <BT_1N3_31@BKTECH.LOCAL>
2018-11-13 10:17:30 -0500

    Initial commit

---
+++ src/Runner.java
@@ -0,0 +1,25 @@
+public class Runner {
+    public void main(String[] args)
+    {
+        int [] arr={0,1,1,1,1,11,1,1,1,1,1,1,1,1,10,1,1,1,1,11,1,1,1,1,1,1,1,1,1};
+
+        boolean done=false;
+        while(!done)
+        {
+           done=true;
+           for(int i=0;i<arr.length-1;i++)
+           {
+               int a=arr[i];
+               int b=arr[i+1];
+               if(a>b)
+               {
+                   done=false;
+                   arr[i]=b;
+                   arr[i+1]=a;
+
+               }
+           }
+        }
+    }
+
+}
